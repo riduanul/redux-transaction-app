df3adac52701b910a4197de78ca37269d3d112ba
Riduanul <riduanul.haque1@gmail.com>
2023-02-17 19:00:39 +0600

    serverlinkadded

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: "https://transaction-app-two.vercel.app/"
+    baseURL: "https://redux-transaction-server998.onrender.com/"
 })
 
 export default axiosInstance;
